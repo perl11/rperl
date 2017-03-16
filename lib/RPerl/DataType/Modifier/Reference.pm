1cf1bfae31b93a8e9ae2906a0d299f1d46bd2dcc
Reini Urban <rurban@cpan.org>
2017-03-16 15:50:14 +0100

    Improve RPerl::DataType::Modifier::Reference
    still perlcritic brutal compat.

--- lib/RPerl/DataType/Modifier/Reference.pm
+++ lib/RPerl/DataType/Modifier/Reference.pm
@@ -3,7 +3,7 @@ package RPerl::DataType::Modifier::Reference;
 use strict;
 use warnings;
 use RPerl::AfterSubclass;
-our $VERSION = 0.002_000;
+our $VERSION = 0.002_001;
 
 # [[[ OO INHERITANCE ]]]
 use parent qw(RPerl::DataType::Modifier);
@@ -12,11 +12,12 @@ use RPerl::DataType::Modifier;
 # [[[ SUB-TYPES ]]]
 # a reference is the location of a data type or data structure;
 # a reference is not a data type, regardless of Perl's internal RV type, use 'unknown' instead
-# NEED FIX???: overload Perl's 'ref' keyword
-package  # hide from PAUSE indexing
-    ref;
-use strict;
-use warnings;
-use parent qw(RPerl::DataType::Modifier::Reference);
+# NEED FIX???: overload Perl's 'ref' keyword via %ref::
+#package  # hide from PAUSE indexing
+#    ref;
+#use strict;
+#use warnings;
+#use parent qw(RPerl::DataType::Modifier::Reference);
+@ref::ISA = qw(RPerl::DataType::Modifier::Reference);
 
 1;  # end of class
